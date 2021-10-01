a8ea9a3e6cd4cc4364c0e3a77257c83f3d930910
artofmayhem <chef_tony_miller@yahoo.com>
2021-10-01 01:25:45 -1000

    final for today

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -12,15 +12,220 @@ const initialFormValues = {
 
 export default function FormContainer() {
   const [formValues, setFormValues] = useState(initialFormValues);
+  const [leadsTraffic, setLeadsTraffic] = useState("");
+  const [qualifiedLeadsFromLeads, setQualifiedLeadsFromLeads] = useState("");
+  const [salesQualifiedLeads, setSalesQualifiedLeads] = useState("");
+  const [costPerLead, setCostPerLead] = useState("");
+  const [costPerQualifiedLead, setCostPerQualifiedLead] = useState("");
+  const [costPerSale, setCostPerSale] = useState("");
+  const [revFromSales, setRevFromSales] = useState("");
+  const [revLessCost, setRevLessCost] = useState("");
+  const [roi, setRoi] = useState("");
+
+  console.log("form values: ", formValues);
+
+  const leadsTrafficHandler = (mtr, conRateTl) => {
+    setLeadsTraffic(mtr / conRateTl);
+    console.log("leads traffic", leadsTraffic);
+  };
+
+  const qualifiedLeadsLeadsHandler = (mtr, conRateTl, conRateQls) => {
+    setQualifiedLeadsFromLeads(mtr / conRateTl / conRateQls);
+    console.log("qualified from leads", qualifiedLeadsFromLeads);
+  };
+
+  const salesQualifiedLeadsHandler = (
+    mtr,
+    conRateTl,
+    conRateLql,
+    conRateQls
+  ) => {
+    setSalesQualifiedLeads(mtr / conRateTl / conRateLql / conRateQls);
+    console.log("sales qualified leads", salesQualifiedLeads);
+  };
+
+  const costPerLeadHandler = (mtr, conRateTl, agCost, inHouseCost) => {
+    setCostPerLead((agCost + inHouseCost) / (mtr / conRateTl));
+    console.log("cost per lead", costPerLead);
+  };
+
+  const costPerQualifiedLeadHandler = (
+    mtr,
+    conRateTl,
+    conRateQls,
+    agCost,
+    inHouseCost
+  ) => {
+    setCostPerQualifiedLead(
+      (agCost + inHouseCost) / (mtr / conRateTl / conRateQls)
+    );
+    // display in dollars and cents
+    console.log(
+      "cost per qualified lead",
+      Math.round(100 * costPerQualifiedLead) / 100
+    );
+  };
+
+  const costPerSaleHandler = (
+    mtr,
+    conRateTl,
+    conRateQls,
+    conRateLql,
+    avgSaleValue,
+    agCost,
+    inHouseCost
+  ) => {
+    setCostPerSale(
+      (agCost + inHouseCost) /
+        ((mtr / conRateTl / conRateLql / conRateQls) * avgSaleValue)
+    );
+    console.log("cost per sale", costPerSale);
+  };
+
+  const revFromSalesHandler = (
+    mtr,
+    conRateTl,
+    conRateQls,
+    conRateLql,
+    avgNetValueSale
+  ) => {
+    setRevFromSales(
+      (mtr / conRateTl / conRateQls / conRateLql) * avgNetValueSale
+    );
+    console.log("revenue from sales", revFromSales);
+  };
+
+  const revLessCostHandler = (
+    mtr,
+    conRateTl,
+    conRateQls,
+    conRateLql,
+    avgNetValueSale,
+    agCost,
+    inHouseCost
+  ) => {
+    setRevLessCost(
+      (mtr / conRateTl / conRateQls / conRateLql) * avgNetValueSale -
+        (agCost + inHouseCost)
+    );
+    console.log("revenue less cost", revLessCost);
+  };
+
+  const roiHandler = (
+    mtr,
+    conRateTl,
+    conRateQls,
+    conRateLql,
+    avgNetValueSale,
+    agCost,
+    inHouseCost
+  ) => {
+    setRoi(
+      (((mtr / conRateTl / conRateQls / conRateLql) * avgNetValueSale -
+        (agCost + inHouseCost)) /
+        (agCost + inHouseCost)) *
+        100
+    );
+    console.log("roi", roi);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     console.log(`name: ${name}, value: ${value}`);
     setFormValues((prevFormValues) => ({ ...prevFormValues, [name]: value }));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    leadsTrafficHandler(formValues.mtr, formValues.conv_rate_TL);
+    qualifiedLeadsLeadsHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.conv_rate_QLS
+    );
+    salesQualifiedLeadsHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.conv_rate_LQL,
+      formValues.conv_rate_QLS
+    );
+    costPerLeadHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.ag_cost,
+      formValues.in_house_cost
+    );
+    costPerQualifiedLeadHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.conv_rate_QLS,
+      formValues.ag_cost,
+      formValues.in_house_cost
+    );
+    costPerSaleHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.conv_rate_QLS,
+      formValues.conv_rate_LQL,
+      formValues.avg_net_value_sale,
+      formValues.ag_cost,
+      formValues.in_house_cost
+    );
+    revFromSalesHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.conv_rate_QLS,
+      formValues.conv_rate_LQL,
+      formValues.avg_net_value_sale
+    );
+    revLessCostHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.conv_rate_QLS,
+      formValues.conv_rate_LQL,
+      formValues.avg_net_value_sale,
+      formValues.ag_cost,
+      formValues.in_house_cost
+    );
+    roiHandler(
+      formValues.mtr,
+      formValues.conv_rate_TL,
+      formValues.conv_rate_QLS,
+      formValues.conv_rate_LQL,
+      formValues.avg_net_value_sale,
+      formValues.ag_cost,
+      formValues.in_house_cost
+    );
+    console.log(
+      "leads traffic",
+      leadsTraffic,
+      "qualified leads from leads",
+      qualifiedLeadsFromLeads,
+      "sales qualified leads",
+      salesQualifiedLeads,
+      "cost per lead",
+      costPerLead,
+      "cost per qualified lead",
+      costPerQualifiedLead,
+      "cost per sale",
+      costPerSale,
+      "revenue from sales",
+      revFromSales,
+      "revenue less cost",
+      revLessCost
+    );
+  };
+
   return (
-    <div className={"w-screen h-auto py-20 bg-blue-300 flex flex-row flex-wrap justify-center align-center"}>
-      <form className="flex flex-col text-2xl text-white text-center border-2 bg-gray-800 border-white w-1/4 py-10">
+    <div
+      className={
+        "w-screen h-auto py-20 bg-blue-300 flex flex-row flex-wrap justify-center align-center"
+      }
+    >
+      <form
+        className="flex flex-col justify-center items-center text-2xl text-white text-center border-2 bg-gray-800 border-white py-10"
+        style={{ width: "30vw", minWidth: 375 }}
+      >
         <h6 className={"text-lg"}>Monthly Traffic Required</h6>
         <input
           name={"mtr"}
@@ -43,7 +248,9 @@ export default function FormContainer() {
             "border-solid border-2 border-gray-600 my-2 p-4 w-72 bg-gray-400 text-black mx-auto text-center"
           }
         />
-        <h6 className={"text-lg"}>Conversion Rate: Lead to Qualified Leads %</h6>
+        <h6 className={"text-lg"}>
+          Conversion Rate: Lead to Qualified Leads %
+        </h6>
         <input
           name={"conv_rate_LQL"}
           type="number"
@@ -98,6 +305,14 @@ export default function FormContainer() {
             "border-solid border-2 border-gray-600 my-2 p-4 w-72 bg-gray-400 text-black mx-auto text-center"
           }
         />
+        <button
+          className={
+            "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-6 rounded w-72"
+          }
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 export default function Nav() {
   return (  
-    <nav className="flex flex-row flex-wrap h-20 text-3xl text-white justify-between items-center bg-transparent" style={{textShadow: '0 0 1rem black'}} >
+    <nav className="flex flex-row flex-wrap h-auto text-2xl text-white justify-between items-center" style={{textShadow: '0 0 1rem black'}} >
         <a className="navbar-brand ml-16" href="https://azella.io">Azella Financial Services</a>
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="flex flex-row flex-wrap justify-evenly align-center w-auto">
